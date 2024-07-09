c103e7ea851d72bf3f2a247bb780dc7d09af94d1
JoeJoeflyn <thaitainguyen336@gmail.com>
2024-07-10 00:45:14 +0700

    feat: paste refactored version into refactored file

--- src/problem3/refactored.tsx
+++ src/problem3/refactored.tsx
@@ -15,7 +15,7 @@ const WalletPage: React.FC<Props> = (props: Props) => {
   const balances = useWalletBalances();
   const prices = usePrices();
 
-  // Prevent re-render multiple times
+// Prevent recreate multiple times
   const getPriority = useCallback((blockchain: any): number => {
     switch (blockchain) {
       case "Osmosis":
@@ -35,16 +35,15 @@ const WalletPage: React.FC<Props> = (props: Props) => {
 
   const sortedBalances = useMemo(() => {
     return balances
-      .filter(
-        (balance: WalletBalance) => lhsPriority > -99 && balance.amount <= 0
-      )
-      .sort(
-        (lhs: WalletBalance, rhs: WalletBalance) =>
-          getPriority(rhs.blockchain) - getPriority(lhs.blockchain)
-      );
-  }, [balances, prices]);
+      .filter((balance: WalletBalance) => lhsPriority > -99 && balance.amount <= 0)
+      .sort((lhs: WalletBalance, rhs: WalletBalance) =>  getPriority(rhs.blockchain) - getPriority(lhs.blockchain));
+  // prices have nothing to do with this func, so i removed it
+  }, [balances
+  //, prices
+  ]);
 
-  // I have no idea why we need these lines ??
+
+// I have no idea why we need these lines ?? 
   //   const formattedBalances = sortedBalances.map((balance: WalletBalance) => {
   //     return {
   //       ...balance,
@@ -71,15 +70,17 @@ const WalletPage: React.FC<Props> = (props: Props) => {
     const formattedAmount = amount.toFixed();
 
     return (
-      <WalletRow
+    // Need to memoize WalletRow to prevent multiple re-renders.
+    -> <WalletRow
         className={row}
-        // When you use index of an array as a key, React will optimize and not render as expected.
-        // What happens in such a scenario can be explained with an example.
-        // Suppose the parent component gets an array of 10 items and renders 10 components based on the array.
-        // Suppose the 5th item is then removed from the array. On the next render the parent will receive an array of 9 items and so React will render 9 components.
-        // This will show up as the 10th component getting removed, instead of the 5th, because React has no way of differentiating between the items based on index.
-        // Therefore always use a unique identifier as a key for components that are rendered from an array of items.
-
+        
+      // When you use index of an array as a key, React will optimize and not render as expected.
+      // What happens in such a scenario can be explained with an example.
+			// Suppose the parent component gets an array of 10 items and renders 10 components based on the array.
+			// Suppose the 5th item is then removed from the array. On the next render the parent will receive an array of 9 items and so React will render 9 components.
+			// This will show up as the 10th component getting removed, instead of the 5th, because React has no way of differentiating between the items based on index.
+			// Therefore always use a unique identifier as a key for components that are rendered from an array of items.
+        
         key={currency}
         amount={amount}
         usdValue={usdValue}
